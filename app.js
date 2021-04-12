e679f68417d293fe586bab0efe97062ca927f21f
Törös Barbara <barbus0303@gmail.com>
2021-04-12 20:54:49 +0200

    update html file and js

---
+++ app.js
@@ -0,0 +1,12 @@
+function calculateFuelToOffload() {
+  var totalDistanceToTravel = prompt('Hány kilométer távolságot kíván megtenni? Adjon meg egy számot!');
+  var averageLitersOverHundred = prompt('Mi az űrhajó átlagos fogyasztása literben? Adjon meg egy számot!');
+  var tankCapacity = prompt('Mi az űrhajó üzemanyagtartályának űrtartalma literben? Adjon meg egy számot!');
+
+  var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100);
+  var fuelToOffload = tankCapacity - requiredFuel;
+  
+  alert('Szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L\n' + 'lecsapolandó mennyiség: ' + fuelToOffload + ' L')
+}
+
+calculateFuelToOffload();
